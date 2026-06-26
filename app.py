d58e3d5ab305152e4102f13582b0a402f324ac2f
saujanyaera <bsaujanya2005@gmail.com>
2026-06-26 20:20:37 +0545

    included .env.example

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-from pipeline import ingest_document
-from rag.document_store import get_vectorstore
+import os
+from dotenv import load_dotenv
+load_dotenv()
 app=Flask(__name__)
 
 
 
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:saujanya@localhost/metadata'
+app.config['SQLALCHEMY_DATABASE_URI']=(
+    f"mysql+pymysql://{os.getenv('DB_USER')}:"
+    f"{os.getenv('DB_PASSWORD')}@"
+    f"{os.getenv('DB_HOST')}/"
+    f"{os.getenv('DB_NAME')}"
+)
 app.config['SQLALCHEMY_TRACK_NOTIFICATIONS']=False
 db=SQLAlchemy(app)
 
