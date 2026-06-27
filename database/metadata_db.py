1a29634ea0f3a06e28010713d56e5a69ed1fb3de
saujanyaera <bsaujanya2005@gmail.com>
2026-06-27 09:40:27 +0545

    .env revised

--- database/metadata_db.py
+++ database/metadata_db.py
@@ -1,15 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
 app_flask=Flask(__name__)
 
 
 
-app_flask.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:saujanya@localhost/metadata'
+
+
+
+app_flask.config['SQLALCHEMY_DATABASE_URI']=(
+    f"mysql+pymysql://{os.getenv('DB_USER')}:"
+    f"{os.getenv('DB_PASSWORD')}@"
+    f"{os.getenv('DB_HOST')}/"
+    f"{os.getenv('DB_NAME')}"
+)
 app_flask.config['SQLALCHEMY_TRACK_NOTIFICATIONS']=False
 db=SQLAlchemy(app_flask)
 
 class Metadata(db.Model):
-    id=db.Column(db.Integer, primary_key=True, nullable=False, auto_increment=True)
+    id=db.Column(db.Integer, primary_key=True, nullable=False)
     author=db.Column(db.String(180), nullable=False)
     creator=db.Column(db.String(180), nullable=False)
     page=db.Column(db.Integer, nullable=False)
